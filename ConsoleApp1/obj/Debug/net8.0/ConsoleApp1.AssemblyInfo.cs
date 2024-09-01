49b7fedc70ed57d675c5adb44ab72a140920567f
sonofbossman <akingbadeeniola452@gmail.com>
2024-09-01 18:15:07 +0100

    Completed the Simple Employee Management System Exercise.

--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b0e887d175909025e50729dd06d62f907b57772e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+091070d0c183d7264221d00990f84f169eed453e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
